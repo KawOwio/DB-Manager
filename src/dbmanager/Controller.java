6fcf7f1caf381e290541e6ae505e0266d0da62b0
Zane Jansone <zane.jansone.zj@gmail.com>
2021-01-27 19:14:42 +0200

    Added JavaFX

---
+++ src/dbmanager/Controller.java
@@ -0,0 +1,9 @@
+package dbmanager;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+	public void printText(ActionEvent event) {
+		System.out.println("Hello");
+	}
+}
--- src/dbmanager/HelloFX.java
+++ src/dbmanager/HelloFX.java
@@ -1,25 +1,59 @@
 package dbmanager;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.stage.Stage;
+
 public class HelloFX extends Application {
-
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
-
+				
+				    @Override
+				    public void start(Stage primaryStage) throws Exception {
+				        primaryStage.setTitle("Hello world Application");
+				        primaryStage.setWidth(300);
+				        primaryStage.setHeight(200);
+				        
+				        FXMLLoader loader = new FXMLLoader();
+				        Parent root = loader.load(getClass().getResource("/sample.fxml"));
+				     
+				        primaryStage.setScene(new Scene(root));
+				        primaryStage.show();
+				
+				    }
+				
+				    public static void main(String[] args) {
+				        launch(args);
+				    }
+	
+//	@Override
+//	public void start(Stage primaryStage) throws Exception {
+//		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//		primaryStage.setTitle("Hello World");
+//		primaryStage.setScene(new Scene(root, 300, 275));
+//		primaryStage.show();
+//	}
+//	
+//	public static void main(String[] args) {
+//		launch(args);
+//	}
 }
+
+
+
+
+
