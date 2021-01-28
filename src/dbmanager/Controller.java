0078548c0ff49913d28256d327d69b9e9c86e34b
Zane Jansone <zane.jansone.zj@gmail.com>
2021-01-28 16:49:43 +0200

    Adding JavaFX features

--- src/dbmanager/Controller.java
+++ src/dbmanager/Controller.java
@@ -1,9 +1,128 @@
 package dbmanager;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 
 public class Controller {
-	public void printText(ActionEvent event) {
-		System.out.println("Hello");
-	}
+	
+//	Side bars buttons
+	
+	@FXML
+	private Button import_mysql_button;
+	
+	@FXML
+	private Button import_excel_button;
+	
+	@FXML
+	private Button import_calc_button;
+	
+	@FXML
+	private Button dbm_button;
+	
+	@FXML
+	private Button import_myfiles_button;
+	
+//	Main windows buttons
+	
+	@FXML
+	private Button import_mysql_button_main;
+	
+	@FXML
+	private Button import_excel_button_main;
+	
+	@FXML
+	private Button import_calc_button_main;
+	
+	@FXML
+	private Button import_word_button_main;
+	
+	@FXML
+	private Button import_writer_button_main;
+	
+//	When side bars button is pressed
+	
+	@FXML
+	private AnchorPane idmyfiles;
+	
+	@FXML
+	private AnchorPane idcalc;
+	
+	@FXML
+	private AnchorPane idexcel;
+	
+	@FXML
+	private AnchorPane idmysql;
+	
+	@FXML
+	private AnchorPane iddbm;
+	
+//	
+	
+//	@FXML
+//	public void initialize() {
+//		my_files_main_bar.setVisible(false);
+//	}
+	
+//	Action on click
+	
+    @FXML
+    void clickdbm(ActionEvent event) {
+    	System.out.println("Main");
+    	idmyfiles.setVisible(false);
+    	idcalc.setVisible(false);
+    	idexcel.setVisible(false);
+    	idmysql.setVisible(false);
+    	iddbm.setVisible(true);
+    }
+	
+    @FXML
+    void clickconnectmysql1(ActionEvent event) {
+    	System.out.println("MySQL");
+    	idmyfiles.setVisible(false);
+    	idcalc.setVisible(false);
+    	idexcel.setVisible(false);
+    	idmysql.setVisible(true);
+    	iddbm.setVisible(false);
+    }
+    
+    @FXML
+    void clickimportexcel1(ActionEvent event) {
+    	System.out.println("Excel");
+    	idmyfiles.setVisible(false);
+    	idcalc.setVisible(false);
+    	idexcel.setVisible(true);
+    	idmysql.setVisible(false);
+    	iddbm.setVisible(false);
+//    	FileChooser fileChooser = new FileChooser();
+//    	fileChooser.setTitle("Open Resource File");
+//    	fileChooser.showOpenDialog(primaryStage);
+    }
+
+    @FXML
+    void clickimportcalc1(ActionEvent event) {
+    	System.out.println("Calc");
+    	idmyfiles.setVisible(false);
+    	idcalc.setVisible(true);
+    	idexcel.setVisible(false);
+    	idmysql.setVisible(false);
+    	iddbm.setVisible(false);
+    }
+    
+    @FXML
+    void clickmyfiles(ActionEvent event) {
+    	System.out.println("My Files");
+    	idmyfiles.setVisible(true);
+    	idcalc.setVisible(false);
+    	idexcel.setVisible(false);
+    	idmysql.setVisible(false);
+    	iddbm.setVisible(false);
+    }
+    
+//    @Override
+//    public void initialize(URL url, ResourceBundle rb) {
+//    	
+//    }
 }
--- src/dbmanager/HelloFX.java
+++ src/dbmanager/HelloFX.java
@@ -14,43 +14,26 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-//import javafx.application.Application;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Parent;
-//import javafx.scene.Scene;
-//import javafx.stage.Stage;
-
 public class HelloFX extends Application {
 				
-				    @Override
-				    public void start(Stage primaryStage) throws Exception {
-				        primaryStage.setTitle("Hello world Application");
-				        primaryStage.setWidth(300);
-				        primaryStage.setHeight(200);
-				        
-				        FXMLLoader loader = new FXMLLoader();
-				        Parent root = loader.load(getClass().getResource("/sample.fxml"));
-				     
-				        primaryStage.setScene(new Scene(root));
-				        primaryStage.show();
-				
-				    }
-				
-				    public static void main(String[] args) {
-				        launch(args);
-				    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello world Application");
+        primaryStage.setWidth(1200);
+        primaryStage.setHeight(800);
+        
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = loader.load(getClass().getResource("/sample.fxml"));
+    
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 	
-//	@Override
-//	public void start(Stage primaryStage) throws Exception {
-//		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-//		primaryStage.setTitle("Hello World");
-//		primaryStage.setScene(new Scene(root, 300, 275));
-//		primaryStage.show();
-//	}
-//	
-//	public static void main(String[] args) {
-//		launch(args);
-//	}
 }
 
 
