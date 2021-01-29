617099f95e3339d9eca508e907d88f28256df7b8
Romans Smirnovs <romans.smirnovs.04@rpg.lv>
2021-01-29 15:46:40 +0200

    time track

---
+++ src/dbmanager/TimeTracking.java
@@ -0,0 +1,37 @@
+package dbmanager;
+
+public class TimeTracking {
+
+	int start;
+	/*
+	 * 
+	 *   object of TimeTracking class should be created when file is opened
+	 *   and elapsedTime() method should be called, when file is being closed
+	 *   
+	 */
+
+	public TimeTracking() {
+		start = (int) System.currentTimeMillis(); 					// captures current time in milliseconds
+	}
+
+	public String elapsedTime() {									// should be called when file is closed
+		int now = (int) System.currentTimeMillis(); 				// captures current time in milliseconds
+		String elapsedTime = "Time spent on editing file: ";
+		int seconds = (now - start) / 1000;							// milliseconds are negotiated, just seconds
+		int minutes = 0;
+		int hours = 0;
+
+		if (seconds >= 3600) {										// checks for hours
+			hours = (seconds / 3600);
+			elapsedTime += hours + " hour(s), ";
+			seconds = seconds % 3600;
+		}
+		if (seconds >= 60) {										// checks for minutes
+			minutes = seconds / 60;
+			elapsedTime += minutes + " minute(s), ";
+			seconds = seconds % 60;
+		}
+		elapsedTime += seconds + " seconds(s).";					// add seconds that are left
+		return elapsedTime;
+	}
+}
