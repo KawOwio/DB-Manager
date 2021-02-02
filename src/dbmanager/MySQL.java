e43be40a48f61d08247eae8464c3abea241e12ae
Romans Smirnovs <romans.smirnovs.04@rpg.lv>
2021-02-02 21:35:35 +0200

    adding new columns to mysql

--- src/dbmanager/MySQL.java
+++ src/dbmanager/MySQL.java
@@ -12,8 +12,8 @@ import java.util.ArrayList;
 public class MySQL {
 	
 	protected Connection conn;
-	private ArrayList<String> columnTypes;
-	private ArrayList<String> columnNames;
+	private ArrayList<String> columnTypes = new ArrayList<>();
+	private ArrayList<String> columnNames = new ArrayList<>();
 	/*
 	* ASK USER TO PROVIDE ALL THE INFO!
 	*/
@@ -21,18 +21,17 @@ public class MySQL {
 //	String password = "dbmapp"; // dynamically provided by user
 //	String databaseName = "dbmanager"; // dynamically provided by user
 //	String databaseName = "test"; // dynamically provided by user
-	String username;
-	String password;
-	String databaseName;
+	String username = "";
+	String password = "";
+	String databaseName = "";
 	String tableName;
 	
 	public MySQL() {
 		
 		// Scanner sc = new Scanner(System.in);
 		try {
-			conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost/?autoReconnect=true&serverTimezone=UTC&characterEncoding=utf8", username,
-					password);
+			String url = "jdbc:mysql://localhost/" + databaseName;
+			conn = DriverManager.getConnection(url, username, password);
 			conn.setAutoCommit(false);
 
 //			databaseName = "dbmanager"; // dynamically provided by user
@@ -57,7 +56,7 @@ public class MySQL {
 			//System.out.println(columnTypes.toString());
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}
+		} 
 	}
 
 	public ArrayList<String> getColumnTypes() {
@@ -67,8 +66,7 @@ public class MySQL {
 	public ArrayList<String> getColumnNames() {
 		return columnNames;
 	}
-
-
+	
 	public ArrayList<String> getValues(String columnName){
 		ArrayList<String> rtnList = new ArrayList<>();
 		String sql = "SELECT " + columnName + " FROM " + databaseName + "." + tableName +"";
@@ -108,10 +106,21 @@ public class MySQL {
 		return rtnList;
 	}
 	
-	public static void main(String[] args) {
+	public void addColumn(String columnName, String dataType) throws SQLException {
+		String url = "jdbc:mysql://localhost/" + databaseName;
+		conn = DriverManager.getConnection(url, username, password);
+		conn.setAutoCommit(false);
+		Statement st = conn.createStatement();
+		String sql = "ALTER TABLE " + tableName + " ADD " + columnName + " " + dataType;
+		System.out.println(sql);
+		st.executeUpdate(sql);
+	}
+	
+	public static void main(String[] args) throws SQLException {
 		MySQL t = new MySQL();
 		for (String name : t.columnNames) {
 		System.out.println(t.getValues(name));
 		}
+		t.addColumn("newColumntn", "varchar(10)");
 	}
 }
