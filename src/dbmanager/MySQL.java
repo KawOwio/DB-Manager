8f2d6014a5dc416836d7a82780cfd65eea76ebc4
Romans Smirnovs <romans.smirnovs.04@rpg.lv>
2021-02-03 14:03:50 +0200

    getting row content

--- src/dbmanager/MySQL.java
+++ src/dbmanager/MySQL.java
@@ -10,13 +10,13 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class MySQL {
-	
+
 	protected Connection conn;
 	private ArrayList<String> columnTypes = new ArrayList<>();
 	private ArrayList<String> columnNames = new ArrayList<>();
 	/*
-	* ASK USER TO PROVIDE ALL THE INFO!
-	*/
+	 * ASK USER TO PROVIDE ALL THE INFO!
+	 */
 //	String username = "dbm"; // dynamically provided by user
 //	String password = "dbmapp"; // dynamically provided by user
 //	String databaseName = "dbmanager"; // dynamically provided by user
@@ -25,27 +25,27 @@ public class MySQL {
 	String password;
 	String databaseName;
 	String tableName;
-	
+
 	public MySQL(String username, String password, String databaseName, String tableName) {
-		
+
 		this.username = username;
 		this.password = password;
 		this.databaseName = databaseName;
 		this.tableName = tableName;
-		
+
 		// Scanner sc = new Scanner(System.in);
 		try {
-			
+
 //			conn = DriverManager.getConnection(
 //					"jdbc:mysql://localhost/?autoReconnect=true&serverTimezone=UTC&characterEncoding=utf8", username,
 //					password);
-			
+
 //			String url = "jdbc:mysql://localhost/" + databaseName;
 //			conn = DriverManager.getConnection(url, username, password);
-			
+
 			String url = "jdbc:mysql://localhost/" + databaseName;
 			conn = DriverManager.getConnection(url, username, password);
-			
+
 			conn.setAutoCommit(false);
 
 //			databaseName = "dbmanager"; // dynamically provided by user
@@ -56,7 +56,8 @@ public class MySQL {
 			ResultSet rs = st.executeQuery("SELECT * from " + databaseName + "." + tableName);
 			ResultSetMetaData rsmd = (ResultSetMetaData) rs.getMetaData();
 			int numOfColumns = rsmd.getColumnCount();
-			//System.out.println("Num of columns: " + numOfColumns); // for testing purposes
+			// System.out.println("Num of columns: " + numOfColumns); // for testing
+			// purposes
 			columnTypes = new ArrayList<>();
 			columnNames = new ArrayList<>();
 			for (int i = 1; i <= numOfColumns; i++) {
@@ -64,43 +65,44 @@ public class MySQL {
 				String dataTypeOfColumn = rsmd.getColumnTypeName(i);
 				columnTypes.add(dataTypeOfColumn);
 				columnNames.add(columnName);
-				//System.out.println(columnName + " has data type " + dataTypeOfColumn); // for testing purposes
+				// System.out.println(columnName + " has data type " + dataTypeOfColumn); // for
+				// testing purposes
 
 			}
-			//System.out.println(columnTypes.toString());
+			// System.out.println(columnTypes.toString());
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} 
+		}
 	}
 
 	public ArrayList<String> getColumnTypes() {
 		return columnTypes;
 	}
-	
+
 	public ArrayList<String> getColumnNames() {
 		return columnNames;
 	}
-	
-	public ArrayList<String> getValues(String columnName){
+
+	public ArrayList<String> getValues(String columnName) {
 		ArrayList<String> rtnList = new ArrayList<>();
-		String sql = "SELECT " + columnName + " FROM " + databaseName + "." + tableName +"";
-		System.out.println(sql);
+		String sql = "SELECT " + columnName + " FROM " + databaseName + "." + tableName + "";
+		// System.out.println(sql);
 		PreparedStatement preparedStatement;
 		try {
 			preparedStatement = conn.prepareStatement(sql);
 			ResultSet results = preparedStatement.executeQuery();
-			
+
 			while (results.next()) {
 				rtnList.add(results.getString(1));
 			}
 			conn.commit();
-			
+
 		} catch (SQLException e) {
 			return rtnList;
 		}
 		return rtnList;
 	}
-	
+
 	public ArrayList<String> getTableNames() {
 		ArrayList<String> rtnList = new ArrayList<>();
 		String sql = "SELECT table_name FROM information_schema.tables WHERE table_schema = '" + databaseName + "';";
@@ -108,18 +110,18 @@ public class MySQL {
 		try {
 			preparedStatement = conn.prepareStatement(sql);
 			ResultSet results = preparedStatement.executeQuery();
-			
+
 			while (results.next()) {
 				rtnList.add(results.getString(1));
 			}
 			conn.commit();
-			
+
 		} catch (SQLException e) {
 			return rtnList;
 		}
 		return rtnList;
 	}
-	
+
 	public void addColumn(String columnName, String dataType) throws SQLException {
 		String url = "jdbc:mysql://localhost/" + databaseName;
 		conn = DriverManager.getConnection(url, username, password);
@@ -129,14 +131,45 @@ public class MySQL {
 		System.out.println(sql);
 		st.executeUpdate(sql);
 	}
-	
-//	public static void main(String[] args) throws SQLException {
-//		MySQL t = new MySQL(username, password, databaseName, tableName);
-//		for (String name : t.columnNames) {
-//		System.out.println(t.getValues(name));
-//		}
-//		t.addColumn("newColumntn", "varchar(10)");
-//	}
+
+	public void getRowContent(String columnName, String data) throws SQLException {
+		String url = "jdbc:mysql://localhost/" + databaseName;
+		conn = DriverManager.getConnection(url, username, password);
+		conn.setAutoCommit(false);
+		
+		ArrayList<String> rowContent = new ArrayList<>();
+		int rowIndex = 0;
+		ArrayList<String> columnValues = getValues(columnName);
+		for (int i = 0; i < columnValues.size(); i++) {
+			if (data.equals((columnValues).get(i))) {
+				rowIndex = i;
+			}
+		}
+		for (String colName : columnNames) {
+			rowContent.add(getValues(colName).get(rowIndex));
+		}
+
+		ArrayList<ArrayList<String>> allTheInfo = new ArrayList<>();
+		allTheInfo.add(columnNames);
+		allTheInfo.add(columnTypes);
+		allTheInfo.add(rowContent);
+		
+		// for (int i = 0; i < columnNames.size(); i++) {
+		// 	
+		// System.out.println(allTheInfo.get(0).get(i));
+		// System.out.println(allTheInfo.get(1).get(i));
+		// System.out.println(allTheInfo.get(2).get(i));
+		// System.out.println();
+		// }
+	}
+
+ 	public static void main(String[] args) throws SQLException {
+ 		MySQL t = new MySQL("dbm", "dbmapp", "dbmanager", "Info");
+ 		t.addColumn("newblablabla", "varchar(50)");
+ 		// t.addColumn("newblablabla", "int");
+ 		// t.addColumn("newblablabla", "decimal(50, 2)");
+ 		// t.getRowContent("name", "name2");
+ 	}
 
 //	public static void main(String[] args) throws SQLException {
 //		MySQL t = new MySQL();
@@ -148,5 +181,5 @@ public class MySQL {
 //		}
 //		t.addColumn("newColumntn", "varchar(10)");
 //	}
-	
+
 }
